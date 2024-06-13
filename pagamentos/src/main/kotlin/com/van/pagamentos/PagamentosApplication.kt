16bc6bbaf2ed99a85947cbf3c36f1e0a71e0a908
rodrigo <rodrigopsps2015@gmail.com>
2024-06-13 14:40:03 -0300

    servico pagamentos

---
+++ pagamentos/src/main/kotlin/com/van/pagamentos/PagamentosApplication.kt
@@ -0,0 +1,11 @@
+package com.van.pagamentos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PagamentosApplication
+
+fun main(args: Array<String>) {
+    runApplication<PagamentosApplication>(*args)
+}
