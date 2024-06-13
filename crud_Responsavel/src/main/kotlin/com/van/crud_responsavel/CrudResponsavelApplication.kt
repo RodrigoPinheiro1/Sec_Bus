a06b96caf67266a7bb05f7f7e332dbae4969990e
rodrigo <rodrigopsps2015@gmail.com>
2024-06-13 14:38:45 -0300

    inicio projeto

---
+++ crud_Responsavel/src/main/kotlin/com/van/crud_responsavel/CrudResponsavelApplication.kt
@@ -0,0 +1,11 @@
+package com.van.crud_responsavel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudResponsavelApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudResponsavelApplication>(*args)
+}
---
+++ crud_Responsavel/src/test/kotlin/com/van/crud_responsavel/CrudResponsavelApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.van.crud_responsavel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudResponsavelApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
